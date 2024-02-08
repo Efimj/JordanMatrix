b9dd21051850c468d6613c0e504c43e5bd9ca010
EFFemole <go.netgle@gmail.com>
2024-02-08 16:57:55 +0200

    updated

--- app/src/main/java/com/example/mapp/MainActivity.kt
+++ app/src/main/java/com/example/mapp/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.mapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mapp.ui.theme.MAppTheme
@@ -35,10 +38,14 @@ class MainActivity : ComponentActivity() {
         val scope = rememberCoroutineScope()
 
         BottomSheetScaffold(
+            modifier = Modifier.fillMaxSize(),
             scaffoldState = sheetState,
             sheetContent = {
                 SheetContent(viewModel)
-            })
+            },
+            containerColor = MaterialTheme.colorScheme.background
+        )
+
         {
             PageContent(scope, sheetState, viewModel)
         }
@@ -51,13 +58,45 @@ class MainActivity : ComponentActivity() {
         sheetState: BottomSheetScaffoldState,
         viewModel: SimpleMainScreenViewModel
     ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            Button(onClick = {
-                scope.launch { sheetState.bottomSheetState.expand() }
-            }) {
-                Text("Open bottom sheet")
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            MatrixSizeSetter(viewModel)
+        }
+    }
+
+    @Composable
+    private fun MatrixSizeSetter(viewModel: SimpleMainScreenViewModel) {
+        Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
+            Text(
+                "Set the matrix",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.SemiBold
+            )
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                OutlinedTextField(
+                    modifier = Modifier.weight(1f),
+                    value = viewModel.states.value.matrixRows.toString(),
+                    onValueChange = viewModel::setMatrixRows,
+                    label = {
+                        Text(text = "Rows")
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+                OutlinedTextField(
+                    modifier = Modifier.weight(1f),
+                    value = viewModel.states.value.matrixColumns.toString(),
+                    onValueChange = viewModel::setMatrixColumns,
+                    label = {
+                        Text(text = "Columns")
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
             }
-            Spacer(modifier = Modifier.height(50.dp))
         }
     }
 
--- app/src/main/java/com/example/mapp/viewModel/SimpleMainScreenViewModel.kt
+++ app/src/main/java/com/example/mapp/viewModel/SimpleMainScreenViewModel.kt
@@ -5,12 +5,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 data class MainScreenStates(
-    val output: String = "none"
-)
+    val output: String = "none",
+    val matrixRows: Int = 3,
+    val matrixColumns: Int = 3,
 
-class SimpleMainScreenViewModel: ViewModel() {
+    )
+
+class SimpleMainScreenViewModel : ViewModel() {
     private val _states = mutableStateOf(MainScreenStates())
     val states: State<MainScreenStates> = _states
 
+    fun setMatrixRows(matrixRowsString: String) {
+        val matrixRows = matrixRowsString.toIntOrNull() ?: return
 
+        if (matrixRows >= 1)
+            _states.value = _states.value.copy(matrixRows = matrixRows)
+    }
+
+    fun setMatrixColumns(matrixColumnsString: String) {
+        val matrixColumns = matrixColumnsString.toIntOrNull() ?: return
+
+        if (matrixColumns >= 1)
+            _states.value = _states.value.copy(matrixColumns = matrixColumns)
+    }
 }
