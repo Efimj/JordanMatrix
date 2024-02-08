0b7a9903ea28674e73ae7f35f340117799d9a5cc
EFFemole <go.netgle@gmail.com>
2024-02-08 18:34:05 +0200

    updated

--- app/src/main/java/com/example/mapp/ui/components/MainPageComponents.kt
+++ app/src/main/java/com/example/mapp/ui/components/MainPageComponents.kt
@@ -8,6 +8,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -35,13 +39,21 @@ fun PageContent(
 
 @Composable
 private fun MatrixSizeSetter(viewModel: SimpleMainScreenViewModel) {
+    val isRelated = rememberSaveable { mutableStateOf(false) }
+
+    LaunchedEffect(isRelated.value, viewModel.states.value.matrixRows) {
+        if (isRelated.value) {
+            viewModel.setMatrixColumns(viewModel.states.value.matrixRows.toString())
+        }
+    }
+
     Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
         Text(
             "Set the matrix",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.SemiBold
         )
-        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+        Row(horizontalArrangement = Arrangement.spacedBy(10.dp), verticalAlignment = Alignment.CenterVertically) {
             OutlinedTextField(
                 modifier = Modifier.weight(1f),
                 value = viewModel.states.value.matrixRows.toString(),
@@ -58,11 +70,24 @@ private fun MatrixSizeSetter(viewModel: SimpleMainScreenViewModel) {
                 label = {
                     Text(text = "Columns")
                 },
+                enabled = !isRelated.value,
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Outlined.Link, contentDescription = "Link")
-            }
+            Switch(
+                checked = isRelated.value,
+                onCheckedChange = { isRelated.value = !isRelated.value },
+                thumbContent = if (isRelated.value) {
+                    {
+                        Icon(
+                            imageVector = Icons.Outlined.Link,
+                            contentDescription = null,
+                            modifier = Modifier.size(SwitchDefaults.IconSize),
+                        )
+                    }
+                } else {
+                    null
+                }
+            )
         }
     }
 }
