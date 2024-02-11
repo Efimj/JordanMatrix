60d4304937bf7e0ccfc70a3007f47bfb3837f076
EFFemole <go.netgle@gmail.com>
2024-02-11 13:42:34 +0200

    updated

--- app/src/main/java/com/example/matrix/ui/components/MainPageComponents.kt
+++ app/src/main/java/com/example/matrix/ui/components/MainPageComponents.kt
@@ -1,11 +1,13 @@
 package com.example.matrix.ui.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -24,18 +26,48 @@ fun PageContent(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
             .fillMaxSize()
-            .padding(20.dp),
+            .padding(20.dp)
+            .padding(bottom = 20.dp),
         verticalArrangement = Arrangement.spacedBy(15.dp)
     ) {
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Showcase",
-            style = MaterialTheme.typography.headlineLarge,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold
-        )
-        MatrixSizeSetter(viewModel)
-        MatrixMinMaxValuesSetter(viewModel)
-        MatrixActions(viewModel, sheetState)
+        val configuration = LocalConfiguration.current
+        when (configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Showcase",
+                    style = MaterialTheme.typography.headlineLarge,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+                Row(horizontalArrangement = Arrangement.spacedBy(20.dp)) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(15.dp)
+                    ) {
+                        MatrixSizeSetter(viewModel)
+                        MatrixMinMaxValuesSetter(viewModel)
+                    }
+                    Box(
+                        modifier = Modifier.weight(1f),
+                    ) {
+                        MatrixActions(viewModel, sheetState)
+                    }
+                }
+            }
+
+            else -> {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Showcase",
+                    style = MaterialTheme.typography.headlineLarge,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+                MatrixSizeSetter(viewModel)
+                MatrixMinMaxValuesSetter(viewModel)
+                MatrixActions(viewModel, sheetState)
+            }
+        }
     }
 }
