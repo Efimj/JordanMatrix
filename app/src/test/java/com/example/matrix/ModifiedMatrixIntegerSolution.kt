f7c2be06ce9b46ceb5dfedc4af13a8f2ca20db4a
EFFemole <go.netgle@gmail.com>
2024-03-12 12:54:11 +0200

    updated

--- app/src/test/java/com/example/matrix/ModifiedMatrixIntegerSolution.kt
+++ app/src/test/java/com/example/matrix/ModifiedMatrixIntegerSolution.kt
@@ -65,6 +65,55 @@ class ModifiedMatrixIntegerSolution {
 
         Assert.assertTrue(correct.contentDeepEquals(result.matrix))
         Assert.assertTrue(correctX.contentDeepEquals(resX))
+    }
 
+    @Test
+    fun TVarint1() {
+        println("TVarint1")
+
+        val inputMatrix = arrayOf(
+            arrayOf(1.0, -1.0, 1.0, 1.0, 2.0),
+            arrayOf(1.0, -1.0, 1.0, -1.0, 2.0),
+            arrayOf(-1.0, -1.0, 1.0, 1.0, -2.0),
+            arrayOf(1.0, -1.0, -1.0, 1.0, 2.0),
+            arrayOf(-1.0, 8.0, -1.0, -4.0, 0.0)
+        )
+
+        val correct = arrayOf(
+            arrayOf(0.5, 0.0, 0.0, -0.5, 2.0),
+            arrayOf(0.0, -2.0, 2.0, 1.0, 0.0),
+            arrayOf(0.5, -1.0, 1.0, 0.5, 0.0),
+            arrayOf(-1.0, 0.0, -2.0, 0.0, 0.0),
+            arrayOf(2.5, 4.0, 3.0, 1.5, 2.0),
+        )
+
+        val correctX = arrayOf(2.0, 0.0, 0.0,  0.0)
+
+        val xy = ModifiedMatrixHandler.Companion.XYPositions(
+            cols = arrayOf("x1", "x2", "x3", "x4"),
+            rows = arrayOf("y1", "y2", "y3")
+        )
+
+        val result = findIntegerSolve(matrix = inputMatrix, xy = xy)
+
+        val resX = ModifiedMatrixHandler.findXresults(result)
+        println("Output")
+        println(result.result)
+        result.matrix.let {
+            if (it == null) return
+            printArray(it)
+        }
+
+        println("Correct")
+        printArray(correct)
+        println("Output X")
+        printArray(resX)
+        println()
+        println("Correct X")
+        printArray(correctX)
+        println()
+
+        Assert.assertTrue(correct.contentDeepEquals(result.matrix))
+        Assert.assertTrue(correctX.contentDeepEquals(resX))
     }
 }
