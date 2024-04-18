07f0dd314e1d2d2452735a4111a747e8897f9d75
EFFemole <go.netgle@gmail.com>
2024-04-18 18:52:16 +0300

    updated solveMatrixGame provided Solve In Mixed Strategies

--- app/src/test/java/com/example/matrixGame/MatrixGamesSolverTests.kt
+++ app/src/test/java/com/example/matrixGame/MatrixGamesSolverTests.kt
@@ -1,10 +1,12 @@
 package com.example.matrixGame
 
 import ArrayHelper.Companion.printArray
+import ArrayHelper.Companion.roundArray
 import com.example.matrix.main.MatrixGamesSolver.Companion.solveMatrixGame
 import com.example.matrix.main.ModifiedMatrixHandler.Companion.XYPositions
 import org.junit.Assert
 import org.junit.Test
+import kotlin.math.roundToInt
 
 class MatrixGamesSolverTests {
     @Test
@@ -34,6 +36,7 @@ class MatrixGamesSolverTests {
         println()
         println("Second player solve")
         printArray(solution.secondPlayersSolution)
+        println()
 
         Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
         Assert.assertTrue(correctSecondPlayerSolution.contentDeepEquals(solution.secondPlayersSolution))
@@ -72,6 +75,171 @@ class MatrixGamesSolverTests {
         println()
         println("Game price")
         println(solution.gamePrice)
+        println()
+
+        Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
+        Assert.assertTrue(correctSecondPlayerSolution.contentDeepEquals(solution.secondPlayersSolution))
+    }
+
+    @Test
+    fun matrixGameSolutionTest3() {
+        println()
+        println("matrixGameSolutionTest3")
+
+        val inputMatrix = arrayOf(
+            arrayOf(0.0, 1.0, -1.0),
+            arrayOf(-1.0, 0.0, 1.0),
+            arrayOf(1.0, -1.0, 0.0)
+        )
+
+        val inputXYPositions = XYPositions(
+            cols = arrayOf("x1", "x2", "x3"),
+            rows = arrayOf("y1", "y2", "y3")
+        )
+
+        val correctFirstPlayerSolution = arrayOf(0.33, 0.33, 0.33)
+        val correctSecondPlayerSolution = arrayOf(0.33, 0.33, 0.33)
+
+        val solution = solveMatrixGame(matrix = inputMatrix, xyPositions = inputXYPositions)
+
+        println()
+        println("Output")
+        printArray(solution.outputMatrix)
+        println()
+        println("First player solve")
+        printArray(solution.firstPlayersSolution)
+        println()
+        println("Second player solve")
+        printArray(solution.secondPlayersSolution)
+        println()
+        println("Game price")
+        println(solution.gamePrice)
+        println()
+
+        Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
+        Assert.assertTrue(correctSecondPlayerSolution.contentDeepEquals(solution.secondPlayersSolution))
+    }
+
+    @Test
+    fun matrixGameSolutionTest4() {
+        println()
+        println("matrixGameSolutionTest4")
+
+        val inputMatrix = arrayOf(
+            arrayOf(5.0, 2.0, 7.0),
+            arrayOf(1.0, 4.0, 3.0),
+            arrayOf(6.0, 1.0, 5.0)
+        )
+
+        val inputXYPositions = XYPositions(
+            cols = arrayOf("x1", "x2", "x3"),
+            rows = arrayOf("y1", "y2", "y3")
+        )
+
+        val correctFirstPlayerSolution = arrayOf(0.52, 0.52, 0.0)
+        val correctSecondPlayerSolution = arrayOf(0.33, 0.67, 0.0)
+
+        val solution = solveMatrixGame(matrix = inputMatrix, xyPositions = inputXYPositions)
+
+        roundArray(solution.firstPlayersSolution, 2)
+        roundArray(solution.secondPlayersSolution, 2)
+
+        println()
+        println("Output")
+        printArray(solution.outputMatrix)
+        println()
+        println("First player solve")
+        printArray(solution.firstPlayersSolution)
+        println()
+        println("Second player solve")
+        printArray(solution.secondPlayersSolution)
+        println()
+        println("Game price")
+        println(solution.gamePrice.roundToInt())
+        println()
+
+        Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
+        Assert.assertTrue(correctSecondPlayerSolution.contentDeepEquals(solution.secondPlayersSolution))
+    }
+
+    @Test
+    fun matrixGameSolutionTest5() {
+        println()
+        println("matrixGameSolutionTest5")
+
+        val inputMatrix = arrayOf(
+            arrayOf(2.0, -1.0, 3.0, 3.0),
+            arrayOf(-1.0, 2.0, 2.0, 7.0),
+            arrayOf(1.0, 1.0, 1.0, 2.0)
+        )
+
+        val inputXYPositions = XYPositions(
+            cols = arrayOf("x1", "x2", "x3", "x4"),
+            rows = arrayOf("y1", "y2", "y3")
+        )
+
+        val correctFirstPlayerSolution = arrayOf(0.0, 0.0, 1.0)
+        val correctSecondPlayerSolution = arrayOf(0.66, 0.34, 0.0)
+
+        val solution = solveMatrixGame(matrix = inputMatrix, xyPositions = inputXYPositions)
+
+        roundArray(solution.firstPlayersSolution, 2)
+        roundArray(solution.secondPlayersSolution, 2)
+
+        println()
+        println("Output")
+        printArray(solution.outputMatrix)
+        println()
+        println("First player solve")
+        printArray(solution.firstPlayersSolution)
+        println()
+        println("Second player solve")
+        printArray(solution.secondPlayersSolution)
+        println()
+        println("Game price")
+        println(solution.gamePrice.roundToInt())
+        println()
+
+        Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
+        Assert.assertTrue(correctSecondPlayerSolution.contentDeepEquals(solution.secondPlayersSolution))
+    }
+
+    @Test
+    fun matrixGameSolutionTest6() {
+        println()
+        println("matrixGameSolutionTest6 variant 1")
+
+        val inputMatrix = arrayOf(
+            arrayOf(3.0, 1.0, 1.0),
+            arrayOf(2.0, -2.0, 1.0),
+            arrayOf(-1.0, -3.0, -2.0)
+        )
+
+        val inputXYPositions = XYPositions(
+            cols = arrayOf("x1", "x2", "x3"),
+            rows = arrayOf("y1", "y2", "y3")
+        )
+
+        val correctFirstPlayerSolution = arrayOf(3.0, 1.0, 1.0)
+        val correctSecondPlayerSolution = arrayOf(1.0, -2.0, -3.0)
+
+        val solution = solveMatrixGame(matrix = inputMatrix, xyPositions = inputXYPositions)
+
+        roundArray(solution.firstPlayersSolution, 2)
+        roundArray(solution.secondPlayersSolution, 2)
+
+        println()
+        println("Output")
+        printArray(solution.outputMatrix)
+        println()
+        println("First player solve")
+        printArray(solution.firstPlayersSolution)
+        println()
+        println("Second player solve")
+        printArray(solution.secondPlayersSolution)
+        println()
+        println("Game price")
+        println(solution.gamePrice.roundToInt())
         println()
 
         Assert.assertTrue(correctFirstPlayerSolution.contentDeepEquals(solution.firstPlayersSolution))
