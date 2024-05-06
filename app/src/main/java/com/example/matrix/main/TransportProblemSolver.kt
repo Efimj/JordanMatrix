5075eaf743306651ad57b096bd5cccbe45c9f420
EFFemole <go.netgle@gmail.com>
2024-05-06 21:53:39 +0300

    update

--- app/src/main/java/com/example/matrix/main/TransportProblemSolver.kt
+++ app/src/main/java/com/example/matrix/main/TransportProblemSolver.kt
@@ -48,63 +48,178 @@ class TransportProblemSolver {
     }
 
     private fun potentialStep(problem: TransportProblem, baseArray: Array<Array<Double>>) {
-        val rowsPotential = Array(problem.rowsPossibility.size) { if (it == 0) 0.0 else Double.NaN }
-        val colsPotential = Array(problem.colsNeed.size) { Double.NaN }
+        while (true) {
+            val rowsPotential = Array(problem.rowsPossibility.size) { if (it == 0) 0.0 else Double.NaN }
+            val colsPotential = Array(problem.colsNeed.size) { Double.NaN }
 
-        for (row in baseArray.indices) {
-            for (col in baseArray.first().indices) {
-                if (baseArray[row][col] != 0.0) {
-                    if (rowsPotential[row].isNaN().not()) {
-                        colsPotential[col] = problem.matrix[row][col] - rowsPotential[row]
-                    }
-                    if (colsPotential[col].isNaN().not()) {
-                        rowsPotential[row] = problem.matrix[row][col] - colsPotential[col]
+            while (rowsPotential.any { it.isNaN() } || colsPotential.any { it.isNaN() }) {
+                for (row in baseArray.indices) {
+                    for (col in baseArray.first().indices) {
+                        if (baseArray[row][col] != 0.0) {
+                            if (rowsPotential[row].isNaN().not()) {
+                                colsPotential[col] = problem.matrix[row][col] - rowsPotential[row]
+                            }
+                            if (colsPotential[col].isNaN().not()) {
+                                rowsPotential[row] = problem.matrix[row][col] - colsPotential[col]
+                            }
+                        }
                     }
                 }
             }
-        }
 
-        println()
-        println("Rows potential")
-        printArray(rowsPotential)
-        println()
-        println("Cols potential")
-        printArray(colsPotential)
-        println()
+            println()
+            println("Rows potential")
+            printArray(rowsPotential)
+            println()
+            println("Cols potential")
+            printArray(colsPotential)
+            println()
 
-        val newMatrixPrice = Array(baseArray.size) { Array(baseArray.first().size) { 0.0 } }
+            val newMatrixPrice = Array(baseArray.size) { Array(baseArray.first().size) { 0.0 } }
 
-        for (row in baseArray.indices) {
-            for (col in baseArray.first().indices) {
-                if (baseArray[row][col] == 0.0) {
-                    newMatrixPrice[row][col] = rowsPotential[row] + colsPotential[col]
+            for (row in baseArray.indices) {
+                for (col in baseArray.first().indices) {
+                    if (baseArray[row][col] == 0.0) {
+                        newMatrixPrice[row][col] = rowsPotential[row] + colsPotential[col]
+                    }
                 }
             }
-        }
 
-        println()
-        println("New price matrix without base prices")
-        printArray(newMatrixPrice)
-        println()
+            println()
+            println("New price matrix without base prices")
+            printArray(newMatrixPrice)
+            println()
 
-        val cells = mutableListOf<Cell>()
+            val cells = mutableListOf<Cell>()
 
-        for (row in baseArray.indices) {
-            for (col in baseArray.first().indices) {
-                if (baseArray[row][col] == 0.0 && newMatrixPrice[row][col] > problem.matrix[row][col]) {
-                    cells.add(
-                        Cell(
-                            value = newMatrixPrice[row][col],
-                            row = row,
-                            col = col
+            for (row in baseArray.indices) {
+                for (col in baseArray.first().indices) {
+                    if (baseArray[row][col] == 0.0 && newMatrixPrice[row][col] > problem.matrix[row][col]) {
+                        cells.add(
+                            Cell(
+                                value = newMatrixPrice[row][col],
+                                row = row,
+                                col = col
+                            )
                         )
-                    )
+                    }
+                }
+            }
+
+            if (cells.isEmpty()) {
+                printArray(baseArray)
+                println("END")
+                return
+            }
+
+            val cellToOptimization = cells.maxBy { it.value }
+
+//            if (problem.matrix[cellToOptimization.row][cellToOptimization.col] > cellToOptimization.value) {
+//                println("END")
+//                println(cellToOptimization.value)
+//                return
+//            }
+
+            val path = findConnectedPath(baseArray, cellToOptimization)
+
+            var minimum = baseArray[path[1].first][path[1].second]
+            for (i in 1 until path.size step 2) {
+                val value = baseArray[path[i].first][path[i].second]
+                if (value < minimum) {
+                    minimum = value
+                }
+            }
+
+            for (i in path.indices) {
+                if (i % 2 == 0) {
+                    baseArray[path[i].first][path[i].second] += minimum
+                } else {
+                    baseArray[path[i].first][path[i].second] -= minimum
+                }
+            }
+
+            println("New base array")
+            printArray(baseArray)
+            println()
+        }
+    }
+
+    private fun findConnectedPath(matrix: Array<Array<Double>>, start: Cell): List<Pair<Int, Int>> {
+        fun dfs(
+            matrix: Array<Array<Double>>,
+            visited: Array<BooleanArray>,
+            row: Int,
+            col: Int,
+            path: MutableList<Pair<Int, Int>>
+        ) {
+            val numRows = matrix.size
+            val numCols = matrix[0].size
+
+            visited[row][col] = true
+            path.add(row to col)
+
+            val directions = arrayOf(
+                intArrayOf(-1, 0), // up
+                intArrayOf(1, 0),  // down
+                intArrayOf(0, -1), // left
+                intArrayOf(0, 1)   // right
+            )
+
+            for (dir in directions) {
+                val newRow = row + dir[0]
+                val newCol = col + dir[1]
+
+                if (newRow < 0 || newRow >= numRows || newCol < 0 || newCol >= numCols || visited[newRow][newCol]) {
+                    continue
+                }
+
+                if (matrix[newRow][newCol] > 0) {
+                    dfs(matrix, visited, newRow, newCol, path)
+                    return
                 }
             }
         }
 
-        val cellToOptimization = cells.maxBy { it.value }
+        fun findClosedPath(matrix: Array<Array<Double>>, startRow: Int, startCol: Int): List<Pair<Int, Int>> {
+            val visited = Array(matrix.size) { BooleanArray(matrix[0].size) }
+            val path = mutableListOf<Pair<Int, Int>>()
 
+            dfs(matrix, visited, startRow, startCol, path)
+
+            return path
+        }
+
+        val res = findClosedPath(matrix, start.row, start.col)
+        val resCut = removeIntermediatePathValues(res)
+
+        println("Vertices")
+        println(resCut)
+        println()
+
+        return resCut
+    }
+
+    private fun removeIntermediatePathValues(res: List<Pair<Int, Int>>): MutableList<Pair<Int, Int>> {
+        val resCut = mutableListOf<Pair<Int, Int>>()
+
+        for (element in res) {
+            if (resCut.size >= 2) {
+                val (lastF, lastS) = resCut.last()
+                val (beforeLastF, beforeLastS) = resCut[resCut.size - 2]
+
+                if (lastF == element.first && beforeLastF == element.first) {
+                    resCut[resCut.size - 1] = element
+                    continue
+                }
+
+                if (lastS == element.second && beforeLastS == element.second) {
+                    resCut[resCut.size - 1] = element
+                    continue
+                }
+            }
+            resCut.add(element)
+        }
+        return resCut
     }
 
     data class Cell(
