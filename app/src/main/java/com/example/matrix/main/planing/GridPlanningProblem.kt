a22bd64dc7edbc9a0e3e656a6ac55a2cd8edb0ed
EFFemole <go.netgle@gmail.com>
2024-05-14 21:06:32 +0300

    update

--- app/src/main/java/com/example/matrix/main/planing/GridPlanningProblem.kt
+++ app/src/main/java/com/example/matrix/main/planing/GridPlanningProblem.kt
@@ -5,21 +5,56 @@ class GridPlanningProblem {
         fun findOptimalSolution(tasksList: List<Task>) {
             val tasks = GridPlanningProblem().makeStartTasksList(tasksList)
 
-            GridPlanningProblem().calculateEarlyAndLateStarts(tasks)
+            GridPlanningProblem().calculateEarlyStarts(tasks)
+            GridPlanningProblem().calculateLateFinishes(tasks)
+
 
             tasks.sortBy { it.taskId }
-
             tasks.forEach { task ->
                 println("TASK ${task.taskId}")
-                println("Duration ${task.duration}")
-                println("Early start ${task.earlyStart}")
-                println("Late start ${task.lateFinish}")
+                println("Duration     ${task.duration}")
+                println("Early start  ${task.earlyStart}")
+                println("Early finish ${task.earlyFinish}")
+                println()
+                println("Late start   ${task.lateStart}")
+                println("Late finish  ${task.lateFinish}")
                 println()
             }
         }
     }
 
-    private fun calculateEarlyAndLateStarts(tasks: MutableList<Task>) {
+    private fun calculateLateFinishes(tasks: MutableList<Task>) {
+        val visitedTasksId = mutableListOf<Int>()
+        while (visitedTasksId.size != tasks.size) {
+            val task = tasks.find { task -> task.taskId !in visitedTasksId } ?: break
+            val nextTasks = tasks.filter { task.taskId in it.previous }
+
+            // if the next ones have not yet been solved.
+            if (visitedTasksId.containsAll(nextTasks.map { it.taskId }).not()) continue
+            val taskIndex = tasks.indexOfFirst { it.taskId == task.taskId }
+            visitedTasksId.add(task.taskId)
+
+            // if no tasks after
+            if (nextTasks.isEmpty()) {
+                tasks[taskIndex] = task.copy(lateFinish = task.earlyFinish)
+                continue
+            }
+
+            // for handle tasks after
+            val lateFinish = nextTasks.minOfOrNull { it.earlyFinish } ?: 0
+
+            println("Task ${task.taskId}")
+            println("Next")
+            println(nextTasks.filter { t -> task.previous.contains(t.taskId) })
+            println("Late finish")
+            println(lateFinish)
+            println()
+
+            tasks[taskIndex] = task.copy(lateFinish = lateFinish)
+        }
+    }
+
+    private fun calculateEarlyStarts(tasks: MutableList<Task>) {
         val visitedTasksId = mutableListOf<Int>()
         while (visitedTasksId.size != tasks.size) {
             val task = tasks.find { task -> task.taskId !in visitedTasksId } ?: break
@@ -50,22 +85,55 @@ class GridPlanningProblem {
     }
 
     private fun makeStartTasksList(tasksList: List<Task>): MutableList<Task> {
-        val startTask = Task(
-            taskId = findUniqueInt(tasksList),
-            previous = emptyList(),
-            duration = 0,
-            resources = 0,
-        )
+        var tasks = deepCopyTasks(tasksList).toMutableList()
 
-        val tasks = deepCopyTasks(tasksList).map { task ->
+        val firstTasks = mutableListOf<Int>()
+        tasks.forEach { task ->
             if (task.previous.isEmpty()) {
-                task.copy(previous = listOf(startTask.taskId))
-            } else {
-                task
+                firstTasks.add(task.taskId)
             }
         }
 
-        return (listOf(startTask) + tasks).toMutableList()
+        // Add first task is needed
+        if (firstTasks.size > 1) {
+            val startTaskId = findUniqueInt(tasksList)
+            val startTask = Task(
+                taskId = startTaskId,
+                previous = emptyList(),
+                duration = 0,
+                resources = 0,
+            )
+
+            tasks.forEachIndexed { index, task ->
+                if (task.previous.isEmpty()) {
+                    tasks[index] = task.copy(previous = listOf(startTaskId))
+                }
+            }
+
+            tasks = (listOf(startTask) + tasks).toMutableList()
+        }
+
+        // Adding final task is needed
+        val endTasks = mutableListOf<Int>()
+        tasks.forEach { task ->
+            if (tasks.none { task.taskId in it.previous }) {
+                endTasks.add(task.taskId)
+            }
+        }
+
+        if (endTasks.size > 1) {
+            val endTaskId = findUniqueInt(tasksList)
+            val endTask = Task(
+                taskId = endTaskId,
+                previous = endTasks,
+                duration = 0,
+                resources = 0,
+            )
+
+            tasks = (tasks + listOf(endTask)).toMutableList()
+        }
+
+        return tasks
     }
 
     private fun findUniqueInt(tasks: List<Task>): Int {
